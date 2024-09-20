6dfb794d337414932f2e838941af98c4b68e4abc
Ryan Deisler <ryandeisler@mail.adelphi.edu>
2024-09-20 12:43:04 -0400

    Fix new page routing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
-import Navbar from "@/components/Navbar/Navbar";
+import Navbar from "@/components/Navbar";
 import { Stack } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "../theme";
