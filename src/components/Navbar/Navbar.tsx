06348327726b276bd828aaf1af8591728d06342d
Ryan Deisler <ryandeisler@mail.adelphi.edu>
2024-09-20 12:59:41 -0400

    Add stub event handler for projects button

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,16 @@
 import { Stack, Typography } from "@mui/material";
 import Button from "@/components/Button";
 
+function clickHandler() {
+  console.log(document.URL);
+}
+
 export default function Navbar() {
   return (
     <Stack spacing={2} className="text-center min-w-fit">
       <Typography variant="h6">Ryan Deisler</Typography>
       <Button>Home</Button>
-      <Button>Projects</Button>
+      <Button onClick={clickHandler}>Projects</Button>
       <Button>Contact Me</Button>
     </Stack>
   );
